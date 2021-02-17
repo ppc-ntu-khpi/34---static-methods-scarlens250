57e34569890427199a668323e896235df0c0a1d6
scarlens250 <74828654+scarlens250@users.noreply.github.com>
2021-02-17 12:29:14 +0200

    Update TestResult.java

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,31 +1,13 @@
 package test;
 
 import domain.Exercise;
-import java.util.Arrays;
-import java.util.Scanner;
-
 
 public class TestResult {
 
     public static void main(String[] args) {
 
-        System.out.println("Введіть кількість елементів масиву");
-        int k = inputVal('k');
-        int[]a = new int[k];
-        int i = k/2, j = 1, c = k/2;
-        a[i] = 0;
-
-
-
-        for ( i = k/2+1, j = 1, c = k/2-1;c >= 0; i++ , j++ , c-- ) {
-            a[i] = j;
-            a[c] = j;
-        }
-        String s = Arrays.toString(a);
-        System.out.println(s);
-    }
-        private static int inputVal(int variableName) {
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
+        int m=5;
+        int n=3;
+        System.out.println("Ackerman's function for number"+n+"and" + m+ "is" + Exercise.Calculate(n,m));
     }
 }
